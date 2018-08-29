f99e744bebacf83f5aceeb10f2032624853fb4d6
pankx <764670547@qq.com>
2018-08-29 11:39:16 +0800

    session的使用

---
+++ session/useSession.php
@@ -0,0 +1,18 @@
+<?php
+//开启session
+    session_start();
+    //设置session
+    if(isset($_SESSION["view"])){
+        $_SESSION["view"] = $_SESSION["view"] + 1;
+    }
+
+?>
+
+<html>
+    <body>
+        <?php
+        //获取session
+            echo "访问了".$_SESSION["view"]."次";
+        ?>
+    </body>
+</html>
