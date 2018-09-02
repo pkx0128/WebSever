6612f4d2e5c540bc2d5694238371575ee0a36200
pankx <764670547@qq.com>
2018-09-02 11:27:06 +0800

    创建数据表

--- mysql/create.php
+++ mysql/create.php
@@ -11,6 +11,17 @@
             echo "创建my_db失败".mysqli_error();
         }
     }
+
+    //选取数据库
+    mysqli_select_db($con,"my_db");
+    //创建数据表
+    $sql = " CREATE TABLE persons(FirstName varchar(15),LastName varchar(15),Age int)";
+
+    if(mysqli_query($con,$sql)){
+        echo "创建persons成功";
+    }else{
+        die("创建persons失败".mysqli_error());
+    }
     // 关闭数据库
     mysqli_colose($con);
 ?>
