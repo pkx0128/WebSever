bf418b96d7fdc08a3a846758ce78da99ecd58531
pankx <764670547@qq.com>
2018-09-05 17:21:02 +0800

    实现把表单提交来的数据保存到mysql

---
+++ mysql/form.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <form action="insertFormData.php" method="POST">
+            <p>FirstName:<input type="text" name="FirstName"></p>
+            <p>LastName:<input type="text" name="LastName"></p>
+            <p>Age:<input type="text" name="Age"></p>
+            <p><input type="submit" value="添加"></p>
+
+        </form>
+    </body>
+
+</html>
---
+++ mysql/insertFormData.php
@@ -0,0 +1,21 @@
+<?php
+
+    $firstName=$_POST["FirstName"];
+    $lastName=$_POST["LastName"];
+    $age=$_POST["Age"];
+
+    $con =mysqli_connect("localhost","root","Pankx0128");
+    if(!$con){
+        die("数据库连接失败".mysqli_error());
+    }else{
+        mysqli_select_db($con,"my_db");
+        $sql = "INSERT INTO persons (FirstName,LastName,Age) VALUES ('$firstName','$lastName',$age);";
+        // echo $sql;
+        if(!mysqli_query($con,$sql)){
+            die("数据添加失败".mysqli_error());
+        }else{
+            echo "成功插入一条数据";
+        }
+    }
+    mysqli_close($con);
+?>
