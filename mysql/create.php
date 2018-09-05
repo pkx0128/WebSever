abdbd88ce305f6691303061ebe7e0a84f7d10027
pankx <764670547@qq.com>
2018-09-05 10:35:56 +0800

    创建数据库和带主键的数据表

--- mysql/create.php
+++ mysql/create.php
@@ -15,7 +15,7 @@
     //选取数据库
     mysqli_select_db($con,"my_db");
     //创建数据表
-    $sql = " CREATE TABLE persons(FirstName varchar(15),LastName varchar(15),Age int)";
+    $sql = " CREATE TABLE persons(personID int NOT NULL AUTO_INCREMENT,PRIMARY KEY(personID),FirstName varchar(15),LastName varchar(15),Age int)";
 
     if(mysqli_query($con,$sql)){
         echo "创建persons成功";
